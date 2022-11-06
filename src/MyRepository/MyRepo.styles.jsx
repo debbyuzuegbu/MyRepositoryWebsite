b736b208bdf70a8b9614be02c06a4dd3c0d775a2
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-11-06 07:59:56 +0100

    designed repositories

--- src/MyRepository/MyRepo.styles.jsx
+++ src/MyRepository/MyRepo.styles.jsx
@@ -1,28 +1,4 @@
 import styled from 'styled-components';
-import {BiCheck, BiDetail, BiDish} from 'react-icons/bi';
-import {Container} from '../Globalstyles';
-
-export const WorksContent = styled.section`
-display: flex;
-justify-content: center;
-flex-direction: column;
-align-items: center;
-margin-top: 3rem;
-`;
-export const WorksContainer = styled(Container)`
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-align-items: center;
-padding: 5rem 8rem;
-color: #333333;
-${Container};
-`;
-
-export const WorksTitle = styled.h2`
-font-size: clamp(2rem, 8.5vw, 5rem);
-font-weight: bold;
-`;
 
 export const WorksCardContent = styled.div`
 margin-top: 1rem;
@@ -78,46 +54,18 @@ transition: all .5s ease;
 &:hover {
     box-shadow:  0px 10px 80px rgba(0, 0, 0, 0.21);
     transform: scale(1.05);
-    background-color: #E38B06;
+    background-color: #43F3E3;
     color: #fff;
     
 }
 `;
 
-export const WorksIconContainer = styled.div`
-width: 9rem;
-height: 9rem;
-border-radius: 50%;
-border: 2px solid #333;
-transition: all .3s ease-out;
-margin-top: 2rem;
-display: flex;
-justify-content: center;
-align-items: center;
-`;
-export const WorksIcon1 = styled(BiCheck)`
-color: #333;
-font-size: 8rem;
-`;
-
-export const WorksIcon2 = styled(BiDetail)`
-color: #333;
-font-size: 6rem;
-`;
-export const WorksIcon3 = styled(BiDish)`
-color: #333;
-font-size: 6rem;
-`;
-
 export const WorksCardTitle = styled.h3`
-/* font-size: 1.5rem; */
 font-weight: bold;
 padding-top: 1rem;
 box-sizing: border-box;
 `;
 
 export const WorksCardText = styled.p`
-/* font-size: 1.0rem; */
-/* padding: 0.5rem 1rem; */
 box-sizing: border-box;
 `;
