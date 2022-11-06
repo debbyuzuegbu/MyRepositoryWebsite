93a3c51cb682c9e9f26e5ee426e49336e89f8f25
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-11-06 09:54:48 +0100

    added singleRepo

--- src/MyRepository/SingleRepo.jsx
+++ src/MyRepository/SingleRepo.jsx
@@ -18,17 +18,17 @@ function SingleRepo() {
     fetchSingleRepo();
   }, []);
 
-  return <div style={{padding: "4rem", height: "100vh"}}>{
+  return <div style={{display:"flex", padding: "4rem", height: "100vh", marginLeft: "30%"}}>{
     repo && repo.map(
         (per) => <div key={per.id} style={{backgroundColor: "#0b0e14", color:"#fff", padding: "5rem"}}>
             {per.name}
-            <img style={{height: "10rem"}} alt="Debby's face" src={per.owner.avatar_url}/>
+            <p><img style={{height: "10rem"}} alt="Debby's face" src={per.owner.avatar_url}/></p>
             <p>{per.owner.login}</p>
             <p>{per.owner.visibility}</p>
             <p>Stargazers Count: {per.stargazers_count}stargazers</p>
             <p>Watchers Count: {per.watchers_count}watchers</p>
             <p> Forks: {per.fork}fork</p>
-            <a href={per.svn_url}>view repo</a>
+            <a href={per.svn_url} style={{color: "#fff"}}>view repo</a>
 
         </div>
     )
