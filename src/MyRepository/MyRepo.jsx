78a83e4d35aead3018bafb5cb8e0b49825bc9351
Deborah <debbyuzuegbu@gmail.com>
2023-01-07 03:06:49 +0100

    changed the position of use effect

--- src/MyRepository/MyRepo.jsx
+++ src/MyRepository/MyRepo.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import axios from "axios";
 import { MyContextApi } from "../App";
-import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import {
